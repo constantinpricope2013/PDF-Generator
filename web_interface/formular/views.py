5b4e595a790384a1e1a47c378aa96408e17ce9c4
cpr <constantin.pricope@computaris.com>
2020-05-07 16:59:56 +0300

    change text from view

--- web_interface/formular/views.py
+++ web_interface/formular/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http  import  HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>This is a simple form.</h1>")
+    return HttpResponse("<h1>This should be on azure.</h1>")
