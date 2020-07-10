3361e42d8a31fe7827d6208aa668d74b4fe4c53d
cpr <ext-comp-cp@ad.datcon.co.uk>
2020-07-10 20:30:32 +0300

    slight modifications

--- createpdf.py
+++ createpdf.py
@@ -1,7 +1,27 @@
+# ###################################
+# Help
+def drawMyRuler(pdf):
+    pdf.drawString(100,810, 'x100')
+    pdf.drawString(200,810, 'x200')
+    pdf.drawString(300,810, 'x300')
+    pdf.drawString(400,810, 'x400')
+    pdf.drawString(500,810, 'x500')
+
+    pdf.drawString(10,100, 'y100')
+    pdf.drawString(10,200, 'y200')
+    pdf.drawString(10,300, 'y300')
+    pdf.drawString(10,400, 'y400')
+    pdf.drawString(10,500, 'y500')
+    pdf.drawString(10,600, 'y600')
+    pdf.drawString(10,700, 'y700')
+    pdf.drawString(10,800, 'y800')  
+
+
+
 # Content
 file_name = 'result.pdf'
 document_title = 'Titlu documentului!'
-title = 'Titlu per pagina.  F'
+title = 'Titlu per pagina.  Final'
 subtitle = 'Subtitlu'
 
 from reportlab.pdfgen import canvas
@@ -10,7 +30,9 @@ pdf = canvas.Canvas(file_name)
 #set title
 pdf.setTitle(document_title)
 #write in pdf
-pdf.drawString(0, 0, title)
+#pointers:
+drawMyRuler(pdf)
+pdf.drawString(200, 200, title)
 
 
 
